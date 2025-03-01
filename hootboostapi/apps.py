6a66ec2dd7ed8edf141ef9349216ec31e84d9ed1
Trevor Mustoe <trevormustoe0405@gmail.com>
2025-03-01 10:25:39 -0500

    Set up files and switched out some info from the template

--- hootboostapi/apps.py
+++ hootboostapi/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TunaapiConfig(AppConfig):
+class HootBoostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tunaapi'
+    name = 'hootboostapi'
